35ca1bc54f860605a56587d15f4bdcf634c23587
mughz <ghozi1997@gmail.com>
2018-05-15 14:24:35 +0700

    custom adapter

---
+++ app/src/main/java/com/example/mughz/slider/CustomPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.mughz.slider;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+class CustomPagerAdapter extends PagerAdapter {
+    private Context ctx;
+    private LayoutInflater layoutInflater;
+    private Integer[] images = {R.drawable.image1,R.drawable.image2, R.drawable.image3, R.drawable.image4};
+
+    public CustomPagerAdapter(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.iView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+}
